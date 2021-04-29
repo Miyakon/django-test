218edc5b3a1ff81f0f9fce3fc5d4c0377f146da7
Miyakon <Miyako0901t@gmail.com>
2021-04-29 20:08:55 +0900

    add fields attribute to ModelAdmin

--- localhost/locallibrary/catalog/admin.py
+++ localhost/locallibrary/catalog/admin.py
@@ -12,6 +12,7 @@ admin.site.register(Genre)
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -25,4 +26,3 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
-
