bd09d8f45b26c6f263f4ac415e41eae344512135
Miyakon <Miyako0901t@gmail.com>
2021-05-03 07:18:24 +0900

    Add Author List and Detail

--- localhost/locallibrary/catalog/urls.py
+++ localhost/locallibrary/catalog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('author/', views.AuthorListView.as_view(), name='author'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     # path('myurl/<int:fish>', views.my_view, {'my_template_name': 'some_path'}, name='aurl'),
 ]
