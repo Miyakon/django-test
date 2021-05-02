dff7ad99d6b54a307136d1b6c4dd3159cf8b05ba
Miyakon <Miyako0901t@gmail.com>
2021-05-03 02:33:11 +0900

    Fix view

--- localhost/locallibrary/catalog/urls.py
+++ localhost/locallibrary/catalog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('books/<int:pk>', views.BookListView.as_view(), name='book-detail'),
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     # path('myurl/<int:fish>', views.my_view, {'my_template_name': 'some_path'}, name='aurl'),
 ]
