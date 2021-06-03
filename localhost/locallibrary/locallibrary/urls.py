b3f5856e2e3da178d98a75a9ce6a1b0319a58d42
Miyakon <Miyako0901t@gmail.com>
2021-06-03 21:17:44 +0900

    Fix path

--- localhost/locallibrary/locallibrary/urls.py
+++ localhost/locallibrary/locallibrary/urls.py
@@ -66,7 +66,7 @@ router.register(r'users', UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
+urlpatterns += [
     path('api', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
