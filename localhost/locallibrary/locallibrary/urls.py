5382b9c54f6550e1ae8fa37d61609eacf65b98c2
Miyakon <Miyako0901t@gmail.com>
2021-06-04 21:29:33 +0900

    Add api

--- localhost/locallibrary/locallibrary/urls.py
+++ localhost/locallibrary/locallibrary/urls.py
@@ -48,6 +48,7 @@ urlpatterns += [
 
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from catalog import views
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -62,7 +63,8 @@ class UserViewSet(viewsets.ModelViewSet):
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
