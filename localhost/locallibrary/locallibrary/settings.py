7c0a9bedbfa247801372534e865eb5e2283b215b
Miyakon <Miyako0901t@gmail.com>
2021-06-14 20:57:07 +0900

    Add css

--- localhost/locallibrary/locallibrary/settings.py
+++ localhost/locallibrary/locallibrary/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'http://3.23.239.230/catalog/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 )
 
 # Default primary key field type
